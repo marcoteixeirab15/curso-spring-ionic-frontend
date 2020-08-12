3f26b0c6abad73b1d6229b0adcf738ba26a27f79
Marco Barbosa <marco.teixeirab15@gmail.com>
2020-08-12 15:48:03 -0300

    Comecando a implementar autenticacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import {HttpClientModule} from "@angular/common/http";
 import {CategoriaService} from "../services/domain/categoria.service";
 import {ErrorInterceptorProvider} from "../interceptors/error-interceptors";
+import {AuthService} from "../services/auth.service";
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import {ErrorInterceptorProvider} from "../interceptors/error-interceptors";
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    AuthService
   ]
 })
 export class AppModule {}
