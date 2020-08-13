729fb55b1b07ea76fcc82126bdf091f6e09fb361
Marco Barbosa <marco.teixeirab15@gmail.com>
2020-08-13 14:02:40 -0300

    Tratando padrao para outros erros com alert

--- src/interceptors/error-interceptors.ts
+++ src/interceptors/error-interceptors.ts
@@ -2,11 +2,13 @@ import {Injectable} from "@angular/core";
 import {HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {StorageService} from "../services/storage.service";
+import {AlertController} from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(public storage: StorageService) {
+  constructor(public storage: StorageService,
+              public alertController: AlertController) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -17,7 +19,7 @@ export class ErrorInterceptor implements HttpInterceptor {
         if (errorObj.error) {
           errorObj = errorObj.error;
         }
-        if (!errorObj.status){
+        if (!errorObj.status) {
           errorObj = JSON.parse(errorObj)
         }
 
@@ -25,19 +27,50 @@ export class ErrorInterceptor implements HttpInterceptor {
         console.log(errorObj);
 
         switch (errorObj.status) {
+          case 401:
+            this.handle401();
+            break;
+
           case 403:
             this.handle403();
             break;
+
+          default:
+            this.hadleDefaultError(errorObj);
+
         }
 
         return Observable.throw(errorObj);
       }) as any;
   }
 
-  handle403(){
+  handle403() {
     this.storage.setLocalUser(null);
   }
 
+  handle401() {
+    let alert = this.alertController.create({
+      title: 'Erro 401: Falha de autheticação',
+      message: 'Email ou senha incorretos',
+      enableBackdropDismiss: false,
+      buttons: [
+        {text: 'Ok'},
+      ]
+    });
+    alert.present();
+  }
+
+  private hadleDefaultError(errorObj: any) {
+    let alert = this.alertController.create({
+      title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+      message: errorObj.message,
+      enableBackdropDismiss: false,
+      buttons: [
+        {text: 'Ok'},
+      ]
+    });
+    alert.present();
+  }
 }
 
 export const ErrorInterceptorProvider = {
