d72e6f2a42681d012c3cb963a8992006e92b5259
Marco Barbosa <marco.teixeirab15@gmail.com>
2020-08-14 11:37:52 -0300

    Carregando produtos por categoria

---
+++ src/services/domain/produto.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {API_CONFIG} from "../../config/api.config";
+
+@Injectable()
+export class ProdutoService {
+
+  constructor(public http: HttpClient) {
+  }
+
+  findByCategoria(categoria_id: string) {
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/page?categorias=${categoria_id}`);
+  }
+
+}
