6746286cf718d468bf481cb35a6957f29f6f1d71
Marco Barbosa <marco.teixeirab15@gmail.com>
2020-08-14 13:26:45 -0300

    Criando pagina de carrinho de compras

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {ProdutoDTO} from "../../models/produto.dto";
 import {ProdutoService} from "../../services/domain/produto.service";
 import {API_CONFIG} from "../../config/api.config";
+import {CartService} from "../../services/domain/cart.service";
 
 @IonicPage()
 @Component({
@@ -11,11 +12,12 @@ import {API_CONFIG} from "../../config/api.config";
 })
 export class ProdutoDetailPage {
 
-  item: ProdutoDTO;
+  item: any;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public produtoService: ProdutoService) {
+              public produtoService: ProdutoService,
+              public cartService: CartService) {
   }
 
   ionViewDidLoad() {
@@ -34,6 +36,11 @@ export class ProdutoDetailPage {
             this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
           },
           error => {});
+  }
+
+  addToCart(produto: ProdutoDTO) {
+    this.cartService.addProduto(produto);
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
