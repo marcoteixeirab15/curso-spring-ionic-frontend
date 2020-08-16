1cb28c5434931e29f811aeb5f28b64b3f266a02d
Marco Barbosa <marco.teixeirab15@gmail.com>
2020-08-16 18:51:38 -0300

    Infinit scrool

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -13,8 +13,8 @@ export class ProdutoService {
     return  this.http.get(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
   }
 
-  findByCategoria(categoria_id: string) {
-    return this.http.get(`${API_CONFIG.baseUrl}/produtos/page?categorias=${categoria_id}`);
+  findByCategoria(categoria_id : string, page : number = 0, linesPerPage : number = 24) {
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/page?categorias=${categoria_id}&page=${page}&linesPerPage=${linesPerPage}`);
   }
 
   getSmallImageFromBucket(id: string) : Observable<any> {
