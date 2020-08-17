3c314ec933f35a1b7b54273126a9b7cbc605fd3a
Marco Barbosa <marco.teixeirab15@gmail.com>
2020-08-17 16:40:27 -0300

    Testando app no dispositivo

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -25,6 +25,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocaluser();
     if (localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -68,6 +72,20 @@ export class ProfilePage {
       this.cameraOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+          this.picture = null;
+          this.loadData();
+        },
+        error => {
+        });
+  }
+
+  cancel() {
+    this.picture = null;
   }
 
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,13 +4,15 @@ import {Observable} from "rxjs";
 import {ClienteDTO} from "../../models/cliente.dto";
 import {API_CONFIG} from "../../config/api.config";
 import {StorageService} from "../storage.service";
+import {ImageUtilService} from "../imagem-util.service";
 
 @Injectable()
 export class ClienteService {
 
 
   constructor(public http: HttpClient,
-              public storage: StorageService) {
+              public storage: StorageService,
+              public imageUtilService: ImageUtilService) {
   }
 
   findById(id: string) {
@@ -30,6 +32,20 @@ export class ClienteService {
     return this.http.post(
       `${API_CONFIG.baseUrl}/clientes`,
       obj,
+      {
+        observe: 'response',
+        responseType: 'text'
+      }
+    );
+  }
+
+  uploadPicture(picture) {
+    let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+    let formData : FormData = new FormData();
+    formData.set('file', pictureBlob, 'file.png');
+    return this.http.post(
+      `${API_CONFIG.baseUrl}/clientes/picture`,
+      formData,
       {
         observe: 'response',
         responseType: 'text'
